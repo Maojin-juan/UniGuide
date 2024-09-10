b2f3f6edada7ba24633aae172a3513b7ce761de9
Maojin-juan <edwardelric199887@gmail.com>
2024-09-10 20:14:18 +0800

    環境設定完成

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,ejs}"],
+  theme: {
+    extend: {
+      backgroundImage: {},
+
+      fontSize: {},
+
+      colors: {},
+
+      fontFamily: {
+        sans: ["Roboto Mono", "sans-serif"],
+      },
+
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "14px",
+          md: "2.25rem",
+        },
+      },
+
+      screens: {
+        sm: "576px",
+        md: "768px",
+        lg: "992px",
+        xl: "1200px",
+        "2xl": "1400px",
+      },
+    },
+  },
+  plugins: [],
+};
