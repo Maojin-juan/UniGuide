36e94b875833199d6cffe6d484d172845f36dfcb
sonia <soniayjw@gmail.com>
2024-09-14 19:57:39 +0800

    更新：before going 區塊

--- src/main.js
+++ src/main.js
@@ -1,5 +1,39 @@
 import "flowbite/dist/flowbite.min.js";
-import 'boxicons/css/boxicons.min.css';
+import "boxicons/css/boxicons.min.css";
 import "boxicons";
+
 import "./assets/style/tailwind.css";
 import "./assets/style/style.css";
+
+// show more、show less button
+document.getElementById("showButton").addEventListener("click", function () {
+  const textElement = document.getElementById("truncateText");
+
+  // 切換 .expanded，控制內文是否截斷
+  textElement.classList.toggle("expanded");
+
+  // 更新按鈕文字
+  if (textElement.classList.contains("expanded")) {
+    this.textContent = "Show less";
+  } else {
+    this.textContent = "Show more";
+  }
+});
+
+// Read more、read less button
+document.querySelectorAll(".readButton").forEach((button) => {
+  button.addEventListener("click", function () {
+    // 找到相應的 p 元素
+    const textElement = this.previousElementSibling;
+
+    // 切換 .expanded，控制內文是否截斷
+    textElement.classList.toggle("expanded");
+
+    // 更新按鈕文字
+    if (textElement.classList.contains("expanded")) {
+      this.textContent = "Read less";
+    } else {
+      this.textContent = "Read more";
+    }
+  });
+});
