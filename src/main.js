e2571c3143ab979673f360093fe52351b8a6ece7
sonia <soniayjw@gmail.com>
2024-09-16 18:49:17 +0800

    更新：頁4篩選視窗

--- src/main.js
+++ src/main.js
@@ -3,38 +3,6 @@ import "boxicons/css/boxicons.min.css";
 
 import "./assets/style/tailwind.css";
 
-// show more、show less button
-document.getElementById("showButton").addEventListener("click", function () {
-  const textElement = document.getElementById("truncateText");
-
-  // 切換 .expanded，控制內文是否截斷
-  textElement.classList.toggle("expanded");
-
-  // 更新按鈕文字
-  if (textElement.classList.contains("expanded")) {
-    this.textContent = "Show less";
-  } else {
-    this.textContent = "Show more";
-  }
-});
-
-// Read more、read less button
-document.querySelectorAll(".readButton").forEach((button) => {
-  button.addEventListener("click", function () {
-    // 找到相應的 p 元素
-    const textElement = this.previousElementSibling;
-
-    // 切換 .expanded，控制內文是否截斷
-    textElement.classList.toggle("expanded");
-
-    // 更新按鈕文字
-    if (textElement.classList.contains("expanded")) {
-      this.textContent = "Read less";
-    } else {
-      this.textContent = "Read more";
-    }
-  });
-});
-
 import "./js/carousel";
 import "./js/dropdown";
+import "./js/showMoreLessButton";
